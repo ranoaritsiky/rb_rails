087d0b03a561846d0546cea1c2db2bf6e113ba56
ranoa <ranoaritsiky@gmail.com>
2023-09-24 18:17:43 +0300

    [FEAT] create unit test

--- test/models/company_test.rb
+++ test/models/company_test.rb
@@ -1,6 +1,19 @@
 require "test_helper"
 
 class CompanyTest < ActiveSupport::TestCase
+  def setup
+    @company = Company.new(name: "Example Company")
+  end
+
+  test "should be valid" do
+    assert @company.valid?
+  end
+
+  test "name should be present" do
+    @company.name = "Example Company"  # Nom présent
+    assert @company.valid?
+  end
+
   # test "the truth" do
   #   assert true
   # end
