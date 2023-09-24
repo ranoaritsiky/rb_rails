bbd66e7170f157847b60a236e94cff9b31054c06
ranoa <ranoaritsiky@gmail.com>
2023-09-24 22:17:52 +0300

    [FEAT] init dropbox method

--- app/controllers/api/reporters_controller.rb
+++ app/controllers/api/reporters_controller.rb
@@ -1,4 +1,6 @@
 require 'csv'
+require 'dropbox'
+
 # app/controllers/api/reporters_controller.rb
 module Api
   class ReportersController < ApplicationController
@@ -16,8 +18,9 @@ module Api
     def create
       @reporter = Reporter.new(reporter_params)
       if @reporter.save
-        generate_csv(@reporter)
-        render json: @reporter, status: :created
+        file_url = generate_csv(@reporter) 
+        dropbox_link = upload_to_dropbox(file_url) 
+        render json: { reporter: @reporter, dropbox_link: dropbox_link }, status: :created
       else
         render json: @reporter.errors, status: :unprocessable_entity
       end
@@ -52,15 +55,43 @@ module Api
     private
 
     def generate_csv(reporter)
-      # Créez un objet CSV ou utilisez une gemme CSV pour générer le fichier
       csv_data = CSV.generate do |csv|
         csv << ["name", "email"]  # En-têtes de colonne
         csv << [reporter.name, reporter.email]  # Données du reporter
       end
 
       # Enregistrez le fichier CSV localement
-      File.open("reporter_#{reporter.name}.csv", "w") { |file| file.write(csv_data) }
+      local_file_path = "reporter_#{reporter.id}.csv"
+      File.open(local_file_path, "w") { |file| file.write(csv_data) }
+
+      local_file_path
     end
+
+    def upload_to_dropbox(file_path)
+      # Configuration Dropbox
+      access_token = 'sl.BmqVuFXigOLwaImKTk5ASwwW8QH9doS9Jn0ykSYrI7Ox3gnwCNBL6vOh5J-pETxKuFnqLC1xa69NuqeGPPo_7t10aeVT_6oFolCn6WFkynAejbpLB_A4lat8EuQmhq7Dr95B2Dm3Fn8P' 
+      # Créez une instance DropboxClient
+      client = Dropbox::Client.new(access_token)
+
+      # Nom du dossier dans Dropbox où vous souhaitez téléverser les fichiers
+      dropbox_folder = '/'
+
+      # Nom du fichier sur Dropbox (utilisez le nom local du fichier)
+      file_name = File.basename(file_path)
+
+      # Chemin complet dans Dropbox (y compris le nom du dossier)
+      dropbox_path = "#{dropbox_folder}/#{file_name}"
+
+      # Téléversez le fichier vers Dropbox
+      response = client.upload(dropbox_path, File.open(file_path, 'rb'))
+
+      # Obtenez le lien partagé public vers le fichier téléversé
+      shared_link = client.create_shared_link(dropbox_path)
+
+      # Retournez le lien partagé public
+      shared_link.url
+    end
+
 
     def set_reporter
       @reporter = Reporter.find(params[:id])
