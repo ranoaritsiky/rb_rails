2a5fa204df6359e5b03c0d30bd020b52ba262a5b
ranoa <ranoaritsiky@gmail.com>
2023-09-24 19:59:38 +0300

    [FEAT] update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :companies, only: [:index, :show, :create, :update, :destroy]
+    get '/companies/:id', to: 'companies#show', as: 'show_company'
+    put '/companies/:id', to: 'companies#update', as: 'update_company'
+
+    resources :reporters, only: [:index, :show, :create, :update, :destroy]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
