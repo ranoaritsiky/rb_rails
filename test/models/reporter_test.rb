b30d2e5fa0696d3a19e4351712d741dbfe11d14d
Tsiky <tsiky@test.com>
2023-09-25 10:57:31 +0300

    [FEAT] add unit test

--- test/models/reporter_test.rb
+++ test/models/reporter_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 class ReporterTest < ActiveSupport::TestCase
   def setup
     @company = Company.create(name: "Example Company")
-    @reporter = Reporter.new(name: "John Doe", email: "john@example.com")
+    @reporter = Reporter.new(name: "John Doe", email: "john@example.com", company_id: 2)
     @reporter.company = @company
   end
 
@@ -12,7 +12,45 @@ class ReporterTest < ActiveSupport::TestCase
     assert @reporter.valid?
   end
 
-  test "should belong to a company" do
+  test "should belong to a reporter" do
     assert_equal @company, @reporter.company
+  end
+
+  # Test pour créer une nouvelle entreprise
+  test "should create a new reporter" do
+    assert_difference('Reporter.count') do
+      @reporter.save
+    end
+  end
+
+  test "should retrieve reporter by id" do
+    @reporter.save  
+    retrieved_reporter = Reporter.find_by(id: @reporter.id)
+    assert_equal @reporter, retrieved_reporter
+  end
+
+
+  # Test pour mettre à jour les informations d'un gardien existante
+  test "should update an existing reporter" do
+    @reporter.save  
+    updated_name = "Updated reporter Name"
+    updated_email = "rakoto@gmail.com"
+    
+    assert_no_difference('Company.count') do
+      @reporter.update(name: updated_name)
+      @reporter.update(email: updated_email)
+    end
+    
+    assert_equal updated_name, @reporter.reload.name
+  end
+
+
+  # Test pour supprimer une entreprise existante
+  test "should delete an existing reporter" do
+    @reporter.save  # Sauvegarde l'entreprise dans la base de données pour obtenir un ID
+
+    assert_difference('Reporter.count', -1) do
+      @reporter.destroy
+    end
   end
 end
